2857fd112c9ca77a8572412f8da2c05b25efeef4
STP-User-08 <renanvanni13@gmail.com>
2022-06-14 17:29:28 -0300

    Moving pieces

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,13 +1,31 @@
 package application;
 
+import java.util.Scanner;
+
 import chess.ChessMatch;
+import chess.ChessPiece;
+import chess.ChessPosition;
 
 public class Program {
 
 	public static void main(String[] args) {
 
+		Scanner sc = new Scanner(System.in);
 		ChessMatch ch = new ChessMatch();
-		UI.printBoard(ch.getPieces());
+
+		while (true) {
+			UI.printBoard(ch.getPieces());
+			System.out.println();
+			System.out.print("Source: ");
+			ChessPosition source = UI.readChessPosition(sc);
+
+			System.out.println();
+			System.out.print("Target: ");
+			ChessPosition target = UI.readChessPosition(sc);
+		
+			ChessPiece capturedPiece = ch.performChessMove(source, target);
+		}
+
 	}
 
 }
